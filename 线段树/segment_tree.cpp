078c334053d6fbb5247b9272783e212d4a869d0b
GZhonghui <mail@gzher.com>
2020-10-28 17:58:26 +0800

    add segment tree

---
+++ 线段树/segment_tree.cpp
@@ -0,0 +1,15 @@
+#include<algorithm>
+#include<cstdio>
+using namespace std;
+
+class SegmentTree
+{
+
+};
+
+int main()
+{
+
+
+    return 0;
+}
---
+++ 计算几何/最小覆盖圆/circle.cpp
@@ -0,0 +1,17 @@
+#include<algorithm>
+#include<cstdio>
+using namespace std;
+
+class Point
+{
+public:
+    double x,y;
+public:
+    Point(double x=0,double y=0):x(x),y(y){}
+};
+
+int main()
+{
+
+    return 0;
+}
