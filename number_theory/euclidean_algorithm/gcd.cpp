4799a106187af7895ae237ff8489c5558249e1d3
GZhonghui <mail@gzher.com>
2024-02-18 17:35:37 +0900

    euclidean_algorithm

--- number_theory/euclidean_algorithm/gcd.cpp
+++ number_theory/euclidean_algorithm/gcd.cpp
@@ -2,9 +2,17 @@
 #include<cstdio>
 using namespace std;
 
+// 最大公約数
 int gcd(int x,int y)
 {
     return y?gcd(y,x%y):x;
+}
+
+// 最小公倍数
+int lcm(int x,int y)
+{
+    int g=gcd(x,y);
+    return x/g*y;
 }
 
 int main()
