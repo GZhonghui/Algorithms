4af89bf199fb435c60662b6be02b97230a33ccf1
GZhonghui <mail@gzher.com>
2024-02-21 17:19:42 +0900

    inverse_element

---
+++ number_theory/inverse_element/inverse_element.cpp
@@ -0,0 +1,21 @@
+#include<algorithm>
+#include<cstdio>
+
+const int mod=1009; // 素数
+
+// 扩展欧几里得法
+int inverse_element_exgcd(int x)
+{
+
+}
+
+// 快速幂法
+int inverse_element_qpow(int x)
+{
+
+}
+
+int main()
+{
+    return 0;
+}
