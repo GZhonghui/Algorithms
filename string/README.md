11ba4b00217662c65f3c14f29828d2deef7b6af8
GZhonghui <mail@gzher.com>
2024-03-19 20:50:36 +0900

    add suffix automaton

--- string/README.md
+++ string/README.md
@@ -1,2 +1,15 @@
 # 字符串算法
----
+
+## aho_corasick
+AC自动机
+
+## kmp
+
+## manacher
+马拉车
+
+## minimal_representation
+最小表示法
+
+## suffix_automaton
+后缀自动机
---
+++ string/suffix_automaton/suffix_automaton.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<cstring>
+#include<cstdio>
+#include<algorithm>
+using namespace std;
+const int maxlen=1e5+10;
+struct Node
+{
+    int len,link;
+    int next[26];
+    Node(int len=0,int link=0)
+    {
+        this->len=len,this->link=link;
+    }
+};
+Node SAM[maxlen<<1];
+int SAM_Size=0,last=0;
+void SAM_Init()
+{
+    SAM[0].len=0;SAM[0].link=-1;
+    SAM_Size=1;last=0;
+    memset(SAM[0].next,0,sizeof(int)*26);
+}
+void SAM_Push(char x)
+{
+
+}
+int main()
+{
+
+    return 0;
+}
