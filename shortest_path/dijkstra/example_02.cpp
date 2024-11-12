05abb2313257d40f403f86d49819809fc0192535
GZhonghui <mail@gzher.com>
2024-11-12 12:33:19 +0900

    add namespace

--- shortest_path/dijkstra/example_02.cpp
+++ shortest_path/dijkstra/example_02.cpp
@@ -9,9 +9,13 @@
 #include<cstdint>
 #include<cstring>
 
+using namespace std;
 typedef int64_t ll;
 
+namespace Dijkstra {
+
 using namespace std;
+typedef int64_t ll;
 
 const ll maxn = 100000 + 1;
 const ll inf = 1e18;
@@ -59,7 +63,7 @@ public:
             ll x=now.id;
             if(done[x]) continue;
             done[x]=true;
-            for(int i=0;i<G[x].size();i++)
+            for(ll i=0;i<G[x].size();i++)
             {
                 Edge& e=edges[G[x][i]];
                 if(d[e.to]>now.dis+e.dis)
@@ -72,7 +76,7 @@ public:
         }
     }
 
-    ll query(int target)
+    ll query(ll target)
     {
         return d[target];
     }
@@ -86,34 +90,41 @@ protected:
     // recalc distance
     void init()
     {
-        for(int i=1;i<=n;i++) done[i]=false,d[i]=inf;
+        for(ll i=1;i<=n;i++) done[i]=false,d[i]=inf;
     }
 
 protected:
     vector<Edge> edges;
-    vector<int> G[maxn];
+    vector<ll> G[maxn];
     ll n,m,d[maxn];
     bool done[maxn];
 }solver;
+
+} // namespace Dijkstra
 
 int main()
 {
     int n,m;
     scanf("%d %d",&n,&m);
-    solver.init((ll)n);
+    Dijkstra::solver.init((ll)n);
     for(int i=0;i<m;i+=1)
     {
         int x,y,d;
         scanf("%d %d %d",&x,&y,&d);
-        solver.add_edge((ll)x,(ll)y,(ll)d);
-        solver.add_edge((ll)y,(ll)x,(ll)d);
+        Dijkstra::solver.add_edge((ll)x,(ll)y,(ll)d);
+        Dijkstra::solver.add_edge((ll)y,(ll)x,(ll)d);
     }
 
-    ll from_1[maxn],from_n[maxn];
-    solver.calc(1);
-    solver.copy_result(from_1);
-    solver.calc(n);
-    solver.copy_result(from_n);
+    ll from_1[Dijkstra::maxn],from_n[Dijkstra::maxn];
+    Dijkstra::solver.calc(1);
+    Dijkstra::solver.copy_result(from_1);
+    Dijkstra::solver.calc(n);
+    Dijkstra::solver.copy_result(from_n);
+
+    for(int i=1;i<=n;i+=1)
+    {
+        cout << from_1[i]+from_n[i] << endl; // printf("%d\n",from_1[i]+from_n[i]);
+    }
 
     return 0;
 }
