2d6584e593dfa8d3533bdc3d6bc4a2b84ac704e8
GZhonghui <mail@gzher.com>
2024-11-13 13:25:48 +0900

    增加注释

--- binary_indexed_tree/binary_indexed_tree_v2.cpp
+++ binary_indexed_tree/binary_indexed_tree_v2.cpp
@@ -5,21 +5,23 @@
 using namespace std;
 const int maxn=1024;
 struct LowBitTree
-{
+{   
+    // 树状数组求和的区间包括0的话，会出一点小问题，因为lowbit(0) = 0
+    // 如果需要包括0的话，可以在Add和Sum的函数内部统一做一个偏移
     int C[maxn],n;
-    void init(int n=maxn-1) // 待验证：n为支持的数据规模，支持的下标范围是[1,n]
+    void init(int n=maxn-1) // n为支持的数据规模，支持的下标范围是[1,n]
     {
         this->n=n;
         memset(C,0,sizeof(int)*maxn);
     }
     int lowbit(int x){ return x&(-x); }
-    int Sum(int pos) // 待验证：计算Sum[1,pos]
+    int Sum(int pos) // 计算Sum[1,pos]
     {
         int ans=0;
         for(int i=pos;i;i-=lowbit(i)) ans+=C[i];
         return ans;
     }
-    void Add(int pos,int value) // 待验证：在pos的位置增加value
+    void Add(int pos,int value) // 在pos的位置增加value
     {
         for(int i=pos;i<=n;i+=lowbit(i)) C[i]+=value;
     }
