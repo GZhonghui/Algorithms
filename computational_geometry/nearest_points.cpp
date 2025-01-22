2243a088dbd5c82802b9b7f0a08a5e33762027ef
GZhonghui <mail@gzher.com>
2025-01-23 01:13:48 +0900

    [重构] 计算几何

---
+++ computational_geometry/nearest_points.cpp
@@ -0,0 +1,9 @@
+/*
+
+最近点对
+问题描述：
+给定平面上的n个点，求出距离最近的两个点（待验证）
+
+*/
+
+#include "computational_geometry.h"
---
+++ computational_geometry/smallest_circle.cpp
@@ -0,0 +1,19 @@
+/*
+
+最小圆覆盖问题
+给定n个坐标点，求一个半径最小的圆，使得所有的点都在圆内部
+
+*/
+
+#include "computational_geometry.h"
+
+namespace computational_geometry
+{
+
+    // 最小圆覆盖问题
+    Circle2f smallest_circle(const vector<Point2f> &points)
+    {
+        return Circle2f();
+    }
+
+} // namespace computational_geometry
