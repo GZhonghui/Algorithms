69ea82d3703a24d937b9d44acc180bf9f0222d5e
GZhonghui <mail@gzher.com>
2025-02-07 02:14:28 +0900

    [整理] 乘法逆元

--- number_theory/inverse_element.cpp
+++ number_theory/inverse_element.cpp
@@ -1,6 +1,26 @@
+/*
+
+乘法逆元（模意义下乘法运算的逆元）
+建议：如果只需要少数几个数字在一个固定模数下的逆元，不用自己计算，搜一下逆元计算器就可以了
+
+扩展欧几里得法
+原理是求解线性同余方程（Linear Congruence Equation）  
+要求【被求逆元的数字】和【模数】互质
+
+快速幂法
+这种方法要求【模数是素数】（因为使用了费马小定理）
+
+线性求逆元
+在线性时间内，求出[1,n]内所有数字的逆元（比上面两种方法要快）
+
+*/
+
+
 #include <algorithm>
 #include <cstdint>
-#include <cstdio>
+
+namespace inverse_element
+{
 
 typedef int64_t ll;
 
@@ -62,7 +82,4 @@ void inverse_elements(ll n, ll* a, ll* inv, ll p)
     for (int i = 1; i <= n; ++i) inv[i] = sv[i] * s[i - 1] % p;
 }
 
-int main()
-{
-    return 0;
-}
+} // namespace inverse_element
