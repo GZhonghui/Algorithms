70975251a82a9e0628f7276a688be7d4e479a0f2
GZhonghui <mail@gzher.com>
2025-02-15 02:40:57 +0900

    [博弈论] Nim Game

---
+++ game_theory/nim.cpp
@@ -0,0 +1,25 @@
+/*
+
+nim game
+n 堆物品，每堆有 a_i 个，两个玩家轮流取走任意一堆的任意个物品，但不能不取
+取走最后一个物品的人获胜
+
+代码尚未测试
+
+*/
+
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+// copied from: https://oi-wiki.org/math/game-theory/impartial-game/
+namespace nim {
+    using namespace std;
+
+    // 先手是否是必胜状态（是否有必胜策略）
+    bool win(const vector<int> &stones) {
+        int res = 0;
+        for(int i : stones) res ^= i;
+        return res;
+    }
+}
